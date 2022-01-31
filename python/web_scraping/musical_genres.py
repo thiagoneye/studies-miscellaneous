085546a8caaaae87920810be82f0f0d740e9d56c
Thiago Rodrigues <thiagoney@outlook.com>
2022-01-31 03:03:30 -0300

    Initial commit (file musical_genres.py)

---
+++ python/web_scraping/musical_genres.py
@@ -0,0 +1,26 @@
+"""
+Estudo sobre web scraping com python.
+
+Busca de gênero de bandas.
+"""
+
+# Importações
+import requests
+from bs4 import BeautifulSoup
+
+# Entradas
+banda = "Arctic Monkeys"
+
+# Manipulação de Dados
+banda = banda.replace(" ", "_")
+
+# Raspagem da página HTML (estrutura de dados não aninhada)
+wiki = "https://pt.wikipedia.org/wiki/"
+html = requests.get(wiki+banda)
+
+# Criação do documento beautifulsoup (estrutura de dados aninhada)
+soup = BeautifulSoup(html.content, 'html.parser')
+
+# Procura por os termos chaves
+genre = soup.find("td", style="vertical-align: top; text-align: left;")
+# print(genre)
