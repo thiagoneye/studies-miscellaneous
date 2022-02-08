cb6f058ec1e32cb242175e3f372a1fdf7c36d419
Thiago Rodrigues <thiagoney@outlook.com>
2022-02-07 21:14:00 -0300

    musical_genres.py in english

--- python/web_scraping/musical_genres.py
+++ python/web_scraping/musical_genres.py
@@ -15,20 +15,15 @@ banda = "Arctic Monkeys"
 banda = banda.replace(" ", "_")
 
 # Raspagem da página HTML (estrutura de dados não aninhada)
-wiki = "https://pt.wikipedia.org/wiki/"
+wiki = "https://en.wikipedia.org/wiki/"
 html = requests.get(wiki+banda)
 
 # Criação do documento beautifulsoup (estrutura de dados aninhada)
 soup = BeautifulSoup(html.content, 'html.parser')
 
 # Procura por os termos chaves
-genre = soup.find(class_='mw-workspace-container')
-genre = genre.find(class_='mw-content-container')
-genre = genre.find(id='content')
-genre = genre.find(id='bodyContent')
-genre = genre.find(id='mw-content-text')
-genre = genre.find_all(class_='infobox infobox infobox_v2')[0]
-genre = genre.find_all('tr')[5].find_all('a')[1].get_text()
+genre = soup.find_all(class_='hlist hlist-separated')[0]
+genre = genre.find_all('li')[0].get_text()
 
 # Prints
 
