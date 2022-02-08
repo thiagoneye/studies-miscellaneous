37c80b57183a933731bc1775e9dbe1893c58c1dd
Thiago Rodrigues <thiagoney@outlook.com>
2022-02-08 01:28:56 -0300

    Update musical_genres.py

--- python/web_scraping/musical_genres.py
+++ python/web_scraping/musical_genres.py
@@ -9,22 +9,45 @@ import requests
 from bs4 import BeautifulSoup
 
 # Entradas
-banda = "Arctic Monkeys"
+banda = "5 Seconds of Summer"
 
 # Manipulação de Dados
 banda = banda.replace(" ", "_")
 
 # Raspagem da página HTML (estrutura de dados não aninhada)
-wiki = "https://en.wikipedia.org/wiki/"
+wiki = "https://pt.wikipedia.org/wiki/"
 html = requests.get(wiki+banda)
 
 # Criação do documento beautifulsoup (estrutura de dados aninhada)
 soup = BeautifulSoup(html.content, 'html.parser')
 
 # Procura por os termos chaves
-genre = soup.find_all(class_='hlist hlist-separated')[0]
-genre = genre.find_all('li')[0].get_text()
+try:
+    genre = soup.find_all(class_='infobox infobox infobox_v2')[0]
+    genre = genre.find_all('tr')
 
-# Prints
+    for i in range(len(genre)):
+        try:
+            value = genre[i].find(scope='row').get_text()[:9]
+            if value == 'Gênero(s)':
+                genre = genre[i].find_all('a')[1].get_text()
+                get_value = True
+        except:
+            value = "It's not that"
+except:
+    get_value = False
+
+if not get_value:
+    genre = soup.find_all(class_='infobox infobox_v2')[0]
+    genre = genre.find_all('tr')
+
+    for i in range(len(genre)):
+        try:
+            value = genre[i].find(scope='row').get_text()[:9]
+            if value == 'Gênero(s)':
+                genre = genre[i].find_all('a')[1].get_text()
+                get_value = True
+        except:
+            value = "It's not that"
 
 print(genre)
